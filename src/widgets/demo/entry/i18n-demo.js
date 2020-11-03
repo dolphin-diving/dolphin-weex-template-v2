2d63119378e9babd938cbe8437689bf1c2b66215
liangzw7 <1901543053@qq.com>
2020-11-03 14:50:41 +0800

    扩展错误上报

--- src/widgets/demo/entry/i18n-demo.js
+++ src/widgets/demo/entry/i18n-demo.js
@@ -1,4 +1,6 @@
-import App from 'src/widgets/demo/views/i18n-demo'
-import dolphinweex from 'src/js/dolphinweex.js'
+import App from '@/widgets/demo/views/i18n-demo'
+import dolphinweex from '@/js/dolphinweex.js'
+import exceptionReport from '@/js/exceptionReport.js'
 
 Vue.use(dolphinweex)
+Vue.use(exceptionReport)
